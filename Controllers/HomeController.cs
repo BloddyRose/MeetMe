1be67211210b83ab737cac664339f44060757eb3
BloddyRose <bloddy.rose.404@gmail.com>
2020-09-28 20:47:04 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using MeetMe.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace MeetMe.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            //    GetInfo.GmailUsername = "Email";
+            //    GetInfo.GmailPassword = "YourPassword";
+
+            //    GetInfo mailer = new GetInfo
+            //    {
+            //        IsHtml = true
+            //    };
+            //    mailer.Send();
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/GetInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace MeetMe.Models
+{
+    public class GetInfo
+    {
+        public static string GmailUsername { get; set; }
+        public static string GmailPassword { get; set; }
+        public static string GmailHost { get; set; }
+        public static int GmailPort { get; set; }
+        public static bool GmailSSL { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Body { get; set; }
+        public bool IsHtml { get; set; }
+
+        static GetInfo()
+        {
+            GmailHost = "smtp.gmail.com";
+            GmailPort = 25; // Gmail can use ports 25, 465 & 587; but must be 25 for medium trust environment.
+            GmailSSL = true;
+        }
+
+        public void Send()
+        {
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = GmailHost,
+                Port = GmailPort,
+                EnableSsl = GmailSSL,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(GmailUsername, GmailPassword)
+            };
+
+            using (MailMessage message = new MailMessage(GmailUsername, Email))
+            {
+                message.Subject = Name + Email;
+                message.Body = Body;
+                message.IsBodyHtml = IsHtml;
+                smtp.Send(message);
+            }
+        }
+
+    }
+}
